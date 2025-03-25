b56238da0100fb4cbff444cf4fd83d1367bdae6e
Ruslan Rogatkin <ruslan.rogatkin2000@gmail.com>
2025-03-25 15:13:24 +1000

    Product Showcase Widget

---
+++ src/app/widgets/product-showcase/page.tsx
@@ -0,0 +1,9 @@
+import ProductShowcase from "@/lib/shared/components/widgets/ProductShowcase";
+
+export default function ProductShowcasePage() {
+  return (
+    <div>
+      <ProductShowcase />
+    </div>
+  );
+}
